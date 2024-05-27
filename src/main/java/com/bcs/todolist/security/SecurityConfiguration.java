53b81c23c6ef0384099dc16bc3539e6893c92567
Siiri Mangus <siiri.mangus@gmail.com>
2024-05-27 03:33:22 +0300

    Add JWT auth logic, remove migration files

--- src/main/java/com/bcs/todolist/security/SecurityConfiguration.java
+++ src/main/java/com/bcs/todolist/security/SecurityConfiguration.java
@@ -1,64 +1,75 @@
-package com.bcs.todolist.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
-    UserDetailsServiceImpl userDetailsService;
-
-    @Autowired
-    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    public static final String[] AUTH_WHITELIST = {
-            "/v3/api-docs/**", "/configuration/ui/**", "/swagger-resources/**",
-            "/configuration/security/**", "/swagger-ui/**", "/webjars/**"
-    };
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(AUTH_WHITELIST).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-
-        return http.build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-
-        return authenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package com.bcs.todolist.security;
+//
+//import com.bcs.todolist.security.JwtAuthenticationFilter;
+//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+//import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+//import io.swagger.v3.oas.annotations.security.SecurityScheme;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+//@SecurityScheme(type = SecuritySchemeType.HTTP, name = "bearerAuth", scheme = "bearer")
+//@SecurityRequirement(name = "bearerAuth")
+//public class SecurityConfiguration {
+//    UserDetailsServiceImpl userDetailsService;
+//    JwtAuthenticationFilter jwtAuthenticationFilter;
+//
+//    @Autowired
+//    public SecurityConfiguration(com.bcs.todolist.security.UserDetailsServiceImpl userDetailsService, JwtAuthenticationFilter jwtAuthenticationFilter) {
+//        this.userDetailsService = userDetailsService;
+//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+//    }
+//
+//    public static final String[] AUTH_WHITELIST = {
+//            "/v3/api-docs/**", "/configuration/ui/**", "/swagger-resources/**",
+//            "/configuration/security/**", "/swagger-ui/**", "/webjars/**"
+//    };
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+//        http
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers(AUTH_WHITELIST).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .logout((l) -> l.clearAuthentication(true));
+//
+//        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//
+//        authenticationProvider.setUserDetailsService(userDetailsService);
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return authenticationProvider;
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
--- src/main/resources/db/migration/V3__add_persons.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO person (first_name, last_name, role_id, username, password)
-VALUES ('Admin', 'Admin', 3, 'admin', '$2a$10$NVwnULVKaLSZIRonLCPI0O74u/WG0fX8K08oXPZweNevvR3CMYYea')
-;
-
-INSERT INTO person (first_name, last_name, role_id, username, password)
-VALUES ('User', 'User', 4, 'user', '$2a$10$wuxzKjmvhdwj3ZuM.DbMKesWNfo4qu20arAieDbmsGoFgsoQadqrO')
-;
